30c00134305a4446072103e0b4301025c79ffd14
sumtopmus <sumtopmus@users.noreply.github.com>
2017-07-26 13:56:40 -0600

    Fixed an issue with the latest numpy version which does not treat a vector with a single coordinate as a scalar.

--- python/pvtools/pvpFile.py
+++ python/pvtools/pvpFile.py
@@ -215,7 +215,7 @@ class pvpOpen(object):
                 self.pvpFile.seek(self.framePos[frame], os.SEEK_SET)
                 time = np.fromfile(self.pvpFile,np.float64,1)[0]
                 timeList.append(time)
-                numActive = np.fromfile(self.pvpFile,np.uint32,1)
+                numActive = np.fromfile(self.pvpFile,np.uint32,1)[0]
                 currentData = np.fromfile(self.pvpFile,entryPattern,numActive)
                 dataIdx = currentData['index']
                 dataValues = currentData['activation']
